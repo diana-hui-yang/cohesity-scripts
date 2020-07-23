82a3aee7d03acb68be9dfd7fd80495a1653e4a0f
Diana Yang <dianafogler@gmail.com>
2020-07-22 18:23:18 -0700

    Update cohcdbb-log.bash

--- nfs/backup-ora-coh-oim/backup-example/cohcdbb-log.bash
+++ nfs/backup-ora-coh-oim/backup-example/cohcdbb-log.bash
@@ -4,13 +4,14 @@ cohesity_user=oraadmin
 cohesity_domain=sa.corp.cohesity.com
 cohesity_cluster=10.19.2.70
 cohesity_job="snap im"
+oracle_host=orawest2
 oracle_database=cohcdbb
 archive_backup_only=yes
 oracle_mount_prefix=/coh/oraoim
 number_of_mount=4
 retention=14
 
-/home/oracle1/scripts/oim/rman/backup-ora-coh-oim.bash -o $oracle_database -a $archive_backup_only -m $oracle_mount_prefix -n $number_o
+/home/oracle1/scripts/oim/rman/backup-ora-coh-oim.bash -h $oracle_host -o $oracle_database -a $archive_backup_only -m $oracle_mount_prefix -n $number_o
 f_mount -e $retention
 
 if [ $? -ne 0 ]; then
