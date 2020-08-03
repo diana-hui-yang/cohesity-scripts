642f3592e56ef49b5bd51e65c870cd07fbaa86ea
Diana Yang <dianafogler@gmail.com>
2020-08-02 17:41:34 -0700

    Update sbt-download.bash

--- sbt/sbt-download.bash
+++ sbt/sbt-download.bash
@@ -1,11 +1,12 @@
 mkdir rman coh
 cd rman
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/dedup/backup-ora-coh-dedup/backup-ora-coh-dedup.bash
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/dedup/restore-ora-coh-dedup/restore-ora-coh-dedup.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/backup-ora-coh-dedup/backup-ora-coh-dedup.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/restore-ora-coh-dedup/restore-ora-coh-dedup.bash
 chmod 750 backup-ora-coh-dedup.bash
 chmod 750 restore-ora-coh-dedup.bash
 cd ../coh
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/nfs-mount/nfs-mount.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/delete-ora-expired/delete-ora-expired.bash
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/backupNow/backupNow.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/cloneView.py
