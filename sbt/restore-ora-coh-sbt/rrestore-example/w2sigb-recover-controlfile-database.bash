5584bd85497917cab916448c41c4429912c2d1ff
Diana Yang <dianafogler@gmail.com>
2020-08-06 18:56:47 -0700

    Update and rename sbt/restore-ora-coh-dedup/restore-exmaple/w2sigb-recover-controlfile-database.bash to sbt/restore-ora-coh-sbt/rrestore-example/w2sigb-recover-controlfile-database.bash

--- sbt/restore-ora-coh-sbt/rrestore-example/w2sigb-recover-controlfile-database.bash
+++ sbt/restore-ora-coh-sbt/rrestore-example/w2sigb-recover-controlfile-database.bash
@@ -12,7 +12,7 @@ oracle_instance=w2sigb
 restore_controlfile=yes
 vip_file=/home/oracle1/scripts/dedup/vip-list
 view=ora_sbt_restore
-dedup_code_directory=/u01/app/cohesity
+sbt_code=/u01/app/cohesity
 force=yes
 point_in_time="2020-08-02 12:00:00"
 
@@ -29,6 +29,5 @@ sleep 5
 
 # Oracle restore
 /home/oracle1/scripts/dedup/rman/restore-ora-coh-dedup.bash -i $oracle_instance -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view
--s $dedup_code_directory -f $force
-#/home/oracle1/scripts/dedup/rman/restore-ora-coh-dedup.bash -i $oracle_instance -l $restore_controlfile -j $vip_file -v $view -s $dedup_code_direct
-ory -f $force
+-s $sbt_code -f $force
+
