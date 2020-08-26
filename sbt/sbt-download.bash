eb8f2122aacb2b7c7f6d4eb37fd47a2ec4c8c599
Diana Yang <dianafogler@gmail.com>
2020-08-26 10:44:27 -0700

    Update sbt-download.bash

--- sbt/sbt-download.bash
+++ sbt/sbt-download.bash
@@ -1,3 +1,5 @@
+mkdir rman
+cd rman
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/backup-ora-coh-sbt/backup-ora-coh-sbt.bash
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/restore-ora-coh-sbt/duplicate-ora-coh-sbt.bash
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/restore-ora-coh-sbt/restore-ora-coh-sbt.bash
@@ -6,3 +8,4 @@ chmod 750 backup-ora-coh-sbt.bash
 chmod 750 duplicate-ora-coh-sbt.bash
 chmod 750 restore-ora-coh-sbt.bash
 chmod 750 delete-ora-expired.bash
+cd ..
