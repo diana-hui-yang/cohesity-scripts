3aa8952e3cb4aad0f1e6399b28034fdfa3e4e93a
Diana Yang <dianafogler@gmail.com>
2020-08-26 08:51:20 -0700

    Update sbt-download.bash

--- sbt/sbt-download.bash
+++ sbt/sbt-download.bash
@@ -1,24 +1,8 @@
-mkdir rman coh
-
-cd rman
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/backup-ora-coh-sbt/backup-ora-coh-sbt.bash
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/restore-ora-coh-sbt/duplicate-ora-coh-sbt.bash
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/restore-ora-coh-sbt/restore-ora-coh-sbt.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/delete-ora-expired/delete-ora-expired.bash
 chmod 750 backup-ora-coh-sbt.bash
 chmod 750 duplicate-ora-coh-sbt.bash
 chmod 750 restore-ora-coh-sbt.bash
-cd ../coh
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/nfs-mount/nfs-mount.bash
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/delete-ora-expired/delete-ora-expired.bash
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/backupNow/backupNow.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/cloneView.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneDirectory/cloneDirectory.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/deleteView/deleteView.py
-chmod +x nfs-mount.bash
-chmod +x delete-ora-expired.bash
-chmod +x backupNow.py
-chmod +x cloneView.py
-chmod +x cloneDirectory.py
-chmod +x deleteView.py
-cd ..
+chmod 750 delete-ora-expired.bash
