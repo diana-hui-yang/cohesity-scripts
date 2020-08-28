cc7bd7692129475104f04c1a348b93b0206969e1
Diana Yang <dianafogler@gmail.com>
2020-08-27 17:54:14 -0700

    Update dup-cohcdbr1-cohcdbr2.bash

--- sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbr2.bash
+++ sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbr2.bash
@@ -7,8 +7,8 @@ target_oraclesid=cohcdbr2
 vip_file=/home/oracle1/scripts/sbt/vip-list
 view=ora_sbt
 sbt_code=/u01/app/cohesity
-ora_pfile=/home/oracle/scripts/sbt/dup-set-cohcdbr2.ora
+ora_set=/home/oracle/scripts/sbt/dup-set-cohcdbr2.ora
 
 
 echo start Oracle duplication
-/home/oracle/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${rmanlogin}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -f ${ora_pfile} -j $vip_file -v $view -s $sbt_code
+/home/oracle/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${rmanlogin}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -f ${ora_set} -j $vip_file -v $view -s $sbt_code
