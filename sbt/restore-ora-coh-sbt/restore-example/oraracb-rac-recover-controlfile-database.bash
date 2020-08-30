c11fb98786971338e4fd033cbcc9783cbdb477ce
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:21:35 -0700

    Create oraracb-rac-recover-controlfile-database.bash

---
+++ sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recover-controlfile-database.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#this script will not use the orignal Cohesity view of Oracle backups. It will use a cloned view.
+
+oracle_instance=oraracb1
+oracle_db=oraracb
+scanname=orascan1
+restore_controlfile=yes
+vip_file=/home/oracle/scripts/dedup/vip-list
+view=orasbt1
+sbt_code=/u01/app/cohesity
+force=yes
+point_in_time="2020-08-02 12:00:00"
+
+# Oracle restore
+/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+
