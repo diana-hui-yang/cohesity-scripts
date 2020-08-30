69091b8fb8ea343b98dafa407daf28232e32a534
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:31:52 -0700

    Update oraracb-rac-recover-database.bash

--- sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recover-database.bash
+++ sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recover-database.bash
@@ -3,9 +3,9 @@
 oracle_instance=oraracb1
 oracle_db=oraracb
 scanname=orascan1
-vip_file=/home/oracle/scripts/dedup/vip-list
+vip_file=/home/oracle/scripts/sbt/vip-list
 view=orasbt1
 sbt_code=/u01/app/cohesity
 force=yes
 
-/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
