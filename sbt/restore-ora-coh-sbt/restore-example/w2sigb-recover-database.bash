51d2b25864e74f0bc35742901c7a7f57d9b37671
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:30:38 -0700

    Update w2sigb-recover-database.bash

--- sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-database.bash
+++ sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-database.bash
@@ -2,9 +2,9 @@
 
 oracle_instance=w2sigb
 host=orawest
-vip_file=/home/oracle/scripts/dedup/vip-list
+vip_file=/home/oracle/scripts/sbt/vip-list
 view=orasbt1
 sbt_code=/u01/app/cohesity
 force=yes
 
-/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -i $oracle_instance -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -i $oracle_instance -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
