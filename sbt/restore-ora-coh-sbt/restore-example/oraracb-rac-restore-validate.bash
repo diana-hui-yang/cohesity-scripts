2f38615ce39cbf7a6c66e6d0a37ec5fec3992556
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:39:40 -0700

    Update oraracb-rac-restore-validate.bash

--- sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-restore-validate.bash
+++ sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-restore-validate.bash
@@ -7,4 +7,4 @@ vip_file=/home/oracle/scripts/sbt/vip-list
 view=orasbt1
 sbt_code=/u01/app/cohesity
 
-/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code
+/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -j $vip_file -v $view/$scanname/$oracle_instance -s $sbt_code
