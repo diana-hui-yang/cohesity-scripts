9023e8e2c76b95837c298078aa34e4f6a7a54637
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:32:13 -0700

    Update oraracb-rac-recover-controlfile-database.bash

--- sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recover-controlfile-database.bash
+++ sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recover-controlfile-database.bash
@@ -6,12 +6,12 @@ oracle_instance=oraracb1
 oracle_db=oraracb
 scanname=orascan1
 restore_controlfile=yes
-vip_file=/home/oracle/scripts/dedup/vip-list
+vip_file=/home/oracle/scripts/sbt/vip-list
 view=orasbt1
 sbt_code=/u01/app/cohesity
 force=yes
 point_in_time="2020-08-02 12:00:00"
 
 # Oracle restore
-/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
 
