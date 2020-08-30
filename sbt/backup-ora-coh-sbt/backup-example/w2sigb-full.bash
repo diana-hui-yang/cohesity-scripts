b359597430e12c43594063e29b3f165b74938767
Diana Yang <dianafogler@gmail.com>
2020-08-30 15:43:26 -0700

    Update w2sigb-full.bash

--- sbt/backup-ora-coh-sbt/backup-example/w2sigb-full.bash
+++ sbt/backup-ora-coh-sbt/backup-example/w2sigb-full.bash
@@ -5,16 +5,16 @@ cohesity_domain=sa.corp.cohesity.com
 cohesity_cluster=10.19.2.70
 cohesity_job="snap sbt"
 oracle_database=w2sigb
+host=orawest
 archive_backup_only=no
 incremental_level=0
 vip_file=/home/oracle/scripts/sbt/vip-list
-view=ora_sbt
-directory=$view/orawest/w2sigb
+view=orasbt1
 sbt_code=/u01/app/cohesity
 retention=7
 
 # backup-ora-coh-oim.bash script does Oracle backup
-/home/oracle/scripts/sbt/rman/backup-ora-coh-sbt.bash -o $oracle_database -a $archive_backup_only -i $incremental_level -f $vip_file -v $directory -s $sbt_code -e $retention
+/home/oracle/scripts/sbt/rman/backup-ora-coh-sbt.bash -o $oracle_database -a $archive_backup_only -i $incremental_level -f $vip_file -v $view/$host/$oracle_database -s $sbt_code -e $retention
 
 if [ $? -ne 0 ]; then
   echo "SBT full backup failed at " `/bin/date '+%Y%m%d%H%M%S'`
