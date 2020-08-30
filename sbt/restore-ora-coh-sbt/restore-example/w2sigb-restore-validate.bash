41b51e8fe730ba308102478134868bcce63b2b50
Diana Yang <dianafogler@gmail.com>
2020-08-30 15:08:52 -0700

    Update w2sigb-restore-validate.bash

--- sbt/restore-ora-coh-sbt/restore-example/w2sigb-restore-validate.bash
+++ sbt/restore-ora-coh-sbt/restore-example/w2sigb-restore-validate.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 oracle_instance=w2sigb
+host=orawest
 vip_file=/home/oracle1/scripts/dedup/vip-list
-view=ora_sbt_restore
+view=orasbt1
 sbt_code=/u01/app/cohesity
 
-/home/oracle1/scripts/dedup/rman/restore-ora-coh-sbt.bash -i $oracle_instance -j $vip_file -v $view -s $sbt_code
+/home/oracle1/scripts/dedup/rman/restore-ora-coh-sbt.bash -i $oracle_instance -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code
