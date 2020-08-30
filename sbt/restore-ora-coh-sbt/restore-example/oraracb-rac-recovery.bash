daac6083483ccc4cb37e3ebc8775297a579deca1
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:17:24 -0700

    Create oraracb-rac-recovery.bash

---
+++ sbt/restore-ora-coh-sbt/restore-example/oraracb-rac-recovery.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+oracle_instance=oraracb1
+oracle_db=oraracb
+scanname=orascan1
+vip_file=/home/oracle/scripts/dedup/vip-list
+view=orasbt1
+sbt_code=/u01/app/cohesity
+force=yes
+
+/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -h $scanname -i $oracle_instance -d $oracle_db -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
