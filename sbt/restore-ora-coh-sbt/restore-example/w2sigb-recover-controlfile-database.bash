2796747956715e616d1a20a5379031423bc37eff
Diana Yang <dianafogler@gmail.com>
2020-08-30 16:19:17 -0700

    Update w2sigb-recover-controlfile-database.bash

--- sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-controlfile-database.bash
+++ sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-controlfile-database.bash
@@ -5,12 +5,12 @@
 oracle_instance=w2sigb
 host=orawest
 restore_controlfile=yes
-vip_file=/home/oracle1/scripts/dedup/vip-list
+vip_file=/home/oracle/scripts/dedup/vip-list
 view=orasbt1
 sbt_code=/u01/app/cohesity
 force=yes
 point_in_time="2020-08-02 12:00:00"
 
 # Oracle restore
-/home/oracle1/scripts/dedup/rman/restore-ora-coh-sbt.bash -i $oracle_instance -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+/home/oracle/scripts/dedup/rman/restore-ora-coh-sbt.bash -i $oracle_instance -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
 
