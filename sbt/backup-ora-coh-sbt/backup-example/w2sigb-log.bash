c4da590430f67d7747a4698903c70c2d398b5f36
Diana Yang <dianafogler@gmail.com>
2020-09-23 18:17:49 -0700

    Update w2sigb-log.bash

--- sbt/backup-ora-coh-sbt/backup-example/w2sigb-log.bash
+++ sbt/backup-ora-coh-sbt/backup-example/w2sigb-log.bash
@@ -8,7 +8,7 @@ oracle_database=w2sigb
 host=orawest
 archive_backup_only=yes
 vip_file=/home/oracle/scripts/sbt/vip-list
-view=orasbt1
+view=orasbt1/$host/$oracle_database
 sbt_code=/u01/app/cohesity
 retention=7
 
