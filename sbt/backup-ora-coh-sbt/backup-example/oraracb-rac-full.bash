cf3f49aceb4851537922c7e334fb325fef268d51
Diana Yang <dianafogler@gmail.com>
2020-09-23 18:15:09 -0700

    Update oraracb-rac-full.bash

--- sbt/backup-ora-coh-sbt/backup-example/oraracb-rac-full.bash
+++ sbt/backup-ora-coh-sbt/backup-example/oraracb-rac-full.bash
@@ -9,28 +9,9 @@ oracle_database=oraracb
 archive_backup_only=no
 incremental_level=0
 vip_file=/home/oracle/scripts/sbt/vip-list
-view=orasbt1
+view=orasbt1/$scanname/$oracle_database
 sbt_code=/u01/app/cohesity
 retention=7
 
 # backup-ora-coh-oim.bash script does Oracle backup
 /home/oracle/scripts/sbt/rman/backup-ora-coh-sbt.bash -h $scanname -o $oracle_database -a $archive_backup_only -i $incremental_level -f $vip_file -v $view/$scanname/$oracle_database -s $sbt_code -e $retention
-
-if [ $? -ne 0 ]; then
-  echo "SBT full backup failed at " `/bin/date '+%Y%m%d%H%M%S'`
-  exit 1
-else
-  echo "SBT full backup finished at " `/bin/date '+%Y%m%d%H%M%S'`
-fi
-
-sleep 30
-
-# Protect the view
-/home/oracle/scripts/dedup/coh/backupNow.py -v $cohesity_cluster -u $cohesity_user -d $cohesity_domain -j "${cohesity_job}" -k 3 -w
-
-if [ $? -ne 0 ]; then
-  echo "Cohesity snapshot failed at " `/bin/date '+%Y%m%d%H%M%S'`
-  exit 1
-else
-  echo "Cohesity snapshot finished at " `/bin/date '+%Y%m%d%H%M%S'`
-fi
