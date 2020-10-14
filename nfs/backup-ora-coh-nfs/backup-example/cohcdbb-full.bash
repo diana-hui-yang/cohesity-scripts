eb0755ff1eb2cb9cc7c869edb0f1567f47cd9029
Diana Yang <dianafogler@gmail.com>
2020-10-14 13:39:07 -0700

    Update cohcdbb-full.bash

--- nfs/backup-ora-coh-nfs/backup-example/cohcdbb-full.bash
+++ nfs/backup-ora-coh-nfs/backup-example/cohcdbb-full.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-oracle_host=orawest2
+oracle_host=orawest
 oracle_database=cohcdbb
-backup_type=full
+backup_incre=0
 archive_backup_only=no
 oracle_mount_prefix=/coh/oranfs
 number_of_mount=4
 retention=14
 
-
-# backup-ora-coh-oim.bash script does Oracle backup, export three variables (host, backup_dir and backup_time), and create catalog bash
+/home/oracle1/scripts/nfs/rman/backup-ora-coh-nfs.bash -h $oracle_host -o $oracle_database  -a $archive_backup_only -i 0 -m $oracle_mount_prefix -n $number_of_mount -e $retention
