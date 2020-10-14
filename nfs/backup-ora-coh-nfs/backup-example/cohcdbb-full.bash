964a8b88c6a5ec22b1d414fabf93449c6e01d721
Diana Yang <dianafogler@gmail.com>
2020-10-14 13:40:15 -0700

    Update cohcdbb-full.bash

--- nfs/backup-ora-coh-nfs/backup-example/cohcdbb-full.bash
+++ nfs/backup-ora-coh-nfs/backup-example/cohcdbb-full.bash
@@ -2,10 +2,10 @@
 
 oracle_host=orawest
 oracle_database=cohcdbb
-backup_incre=0
+incremental_level=0
 archive_backup_only=no
 oracle_mount_prefix=/coh/oranfs
 number_of_mount=4
 retention=14
 
-/home/oracle1/scripts/nfs/rman/backup-ora-coh-nfs.bash -h $oracle_host -o $oracle_database  -a $archive_backup_only -i 0 -m $oracle_mount_prefix -n $number_of_mount -e $retention
+/home/oracle1/scripts/nfs/rman/backup-ora-coh-nfs.bash -h $oracle_host -o $oracle_database  -a $archive_backup_only -i $incremental_level -m $oracle_mount_prefix -n $number_of_mount -e $retention
