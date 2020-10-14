af9160e597818abb30e13f93ace7588fab28d7c4
Diana Yang <dianafogler@gmail.com>
2020-10-14 13:48:58 -0700

    Create cohcdbb-incre.bash

---
+++ nfs/backup-ora-coh-nfs/backup-example/cohcdbb-incre.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+oracle_host=orawest
+oracle_database=cohcdbb
+incremental_level=1
+archive_backup_only=no
+oracle_mount_prefix=/coh/oranfs
+number_of_mount=4
+retention=14
+
+/home/oracle1/scripts/nfs/rman/backup-ora-coh-nfs.bash -h $oracle_host -o $oracle_database  -a $archive_backup_only -i $incremental_level -m $oracle_mount_prefix -n $number_of_mount -e $retention
