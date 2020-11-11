b7683fe87844f4d3ae9bba8562e3ff75413133d6
Diana Yang <dianafogler@gmail.com>
2020-11-11 15:08:26 -0800

    Update oraracb-rac-full.bash

--- sbt/backup-ora-coh-sbt/backup-example/oraracb-rac-full.bash
+++ sbt/backup-ora-coh-sbt/backup-example/oraracb-rac-full.bash
@@ -2,12 +2,10 @@
 
 scanname=orascan1
 oracle_database=oraracb
-archive_backup_only=no
 incremental_level=0
-vip_file=/home/oracle/scripts/sbt/vip-list
 view=orasbt1/$scanname/$oracle_database
-sbt_code=/u01/app/cohesity
 retention=7
+cohesity_name="cohesity"
 
 # backup-ora-coh-oim.bash script does Oracle backup
-/home/oracle/scripts/sbt/rman/backup-ora-coh-sbt.bash -h $scanname -o $oracle_database -a $archive_backup_only -i $incremental_level -f $vip_file -v $view/$scanname/$oracle_database -s $sbt_code -e $retention
+/home/oracle/scripts/sbt/rman/backup-ora-coh-sbt.bash -h $scanname -o $oracle_database -y "${cohesity_name}" -i $incremental_level -v $view/$scanname/$oracle_database -e $retention
