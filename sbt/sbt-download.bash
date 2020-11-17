7b878873471ecab35d7918afc7abc10c5ec8f429
Diana Yang <dianafogler@gmail.com>
2020-11-17 14:38:25 -0800

    Update sbt-download.bash

--- sbt/sbt-download.bash
+++ sbt/sbt-download.bash
@@ -1,4 +1,5 @@
 mkdir rman coh
+mkdir rman/config rman/lib
 cd rman
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/backup-ora-coh-sbt/backup-ora-coh-sbt.bash
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/sbt/duplicate-ora-coh-sbt/duplicate-ora-coh-sbt.bash
@@ -8,10 +9,7 @@ chmod 750 backup-ora-coh-sbt.bash
 chmod 750 duplicate-ora-coh-sbt.bash
 chmod 750 restore-ora-coh-sbt.bash
 chmod 750 delete-ora-expired.bash
-mkdir config
-touch vip-list
-mkdir lib
-cd ../../coh
+cd ../coh
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/backupNow/backupNow.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/easyScript/storePassword/python/storePassword.py
