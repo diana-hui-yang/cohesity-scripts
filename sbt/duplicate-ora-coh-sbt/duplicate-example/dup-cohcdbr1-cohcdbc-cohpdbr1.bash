2a553f75660222ed801061f259e37be5ca91d6d0
Diana Yang <dianafogler@gmail.com>
2021-01-14 21:44:55 -0800

    Update dup-cohcdbr1-cohcdbc-cohpdbr1.bash

--- sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash
+++ sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-rmanlogin="rman auxiliary / target <user>/<passwd>@<oracle connection string>"
+#catalogconnect="<user>/<passwd>@orawest:/catalog"
+targetconnect="<user>/<passwd>@orawest2/cohcdbr1"
 prod_host=oracle-01
 source_db=cohcdbr1
-source_pdb=cohpdbr1
+source_pdb=cohpdbr2
 target_oraclesid=cohcdbc
-vip_file=/home/oracle/scripts/sbt/vip-list
-view=ora_sbt/$prod_host/$source_d
-sbt_code=/u01/app/cohesity
-ora_set=/home/oracle/scripts/sbt/dup-set-cohpdbr1.ora
+view=ora_sbt/orawest2/cohcdbr1
+cohesity_name="sac01-ftdcoh"
+ora_set=/home/oracle1/scripts/sbt/dup-set-cohpdbr1.ora
+
 
 echo start Oracle duplication
-/home/oracle/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${rmanlogin}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -f ${ora_set} -j $vip_file -v $view -s $sbt_code -c $source_pdb
+/home/oracle1/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${targetconnect}" -y "${cohesity_name}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -f ${ora_set} -v $view -c $source_pdb
