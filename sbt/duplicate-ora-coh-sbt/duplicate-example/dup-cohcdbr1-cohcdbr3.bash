7c42d835af673ec45ce2aa062802343f693f9fea
Diana Yang <dianafogler@gmail.com>
2021-05-05 19:17:08 -0700

    Update and rename dup-cohcdbr1-cohcdbr2.bash to dup-cohcdbr1-cohcdbr3.bash

---
+++ sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbr3.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+targetconnect="<user>/<passwd>@oracle-01/cohcdbr1"
+prod_host=oracle-01
+source_db=cohcdbr1
+target_oraclesid=cohcdbr3
+view=ora_sbt/orawest2/cohcdbr1
+ora_set1=/home/oracle1/scripts/sbt/dup-set1-cohcdbr3.ora
+ora_set2=/home/oracle1/scripts/sbt/dup-set2-cohcdbr3.ora
+cohesity_name="sac01-ftdcoh"
+#catalogconnect="rman/fr8shst8rt@orawest:/catalog"
+
+echo start Oracle duplication
+/home/oracle1/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${targetconnect}" -y "${cohesity_name}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -l ${ora_set1} -i ${ora_set2} -v $view
