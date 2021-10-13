768dbc4c010d84c5c3fadf33b13b83e03f623b7f
Diana Yang <dianafogler@gmail.com>
2021-10-12 18:09:21 -0700

    Update and rename sun-download to sun-oim-download.bash

--- oim/sun-oim-download.bash
+++ oim/sun-oim-download.bash
@@ -1,12 +1,8 @@
 mkdir rman
 cd rman
 mkdir python
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/linux/backup-ora-coh-oim.bash
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/duplicate-ora-coh-oim/duplicate-ora-coh-oim.bash
-curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/restore-ora-coh-oim/restore-ora-coh-oim.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/sun/sbackup-ora-coh-oim.bash
 chmod 750 backup-ora-coh-oim.bash
-chmod 750 duplicate-ora-coh-oim.bash
-chmod 750 restore-ora-coh-oim.bash
 cd python
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneDirectory/cloneDirectory.py
