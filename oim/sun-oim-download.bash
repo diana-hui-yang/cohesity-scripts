1364b45c9bc732e4dd631665c1702062931fce17
Diana Yang <dianafogler@gmail.com>
2021-10-13 11:58:02 -0700

    Update sun-oim-download.bash

--- oim/sun-oim-download.bash
+++ oim/sun-oim-download.bash
@@ -6,7 +6,7 @@ chmod 750 backup-ora-coh-oim.bash
 cd python
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneDirectory/cloneDirectory.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/easyScript/storePassword/python/storePassword.py
+curl -O https://github.com/bseltz-cohesity/scripts/tree/master/python/storePassword
 chmod 750 cloneDirectory.py
 chmod 750 storePassword.py
 cd ..
