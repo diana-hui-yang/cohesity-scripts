0502832015b6eeea8b610c6813c0912051746282
Diana Yang <dianafogler@gmail.com>
2021-10-25 21:02:44 -0700

    Update sun-oim-download.bash

--- oim/sun-oim-download.bash
+++ oim/sun-oim-download.bash
@@ -6,7 +6,11 @@ if [[ ! -d python ]]; then
   mkdir python
 fi
 curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/sun/sbackup-ora-coh-oim.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/duplicate-ora-coh-nfs/sun/sun-duplicate-ora-coh-nfs.bash
+curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/restore-ora-coh-nfs/sun/sun-restore-ora-coh-nfsbash
 chmod 750 sbackup-ora-coh-oim.bash
+chmod 750 sun-duplicate-ora-coh-nfs.bash
+chmod 750 sun-restore-ora-coh-nfs.bas
 cd python
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneDirectory/cloneDirectory.py
