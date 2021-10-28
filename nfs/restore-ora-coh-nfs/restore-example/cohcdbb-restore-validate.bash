96591a2b4a9bb3497e5d93f34c3151bca906baba
Diana Yang <dianafogler@gmail.com>
2021-10-27 17:12:48 -0700

    Update cohcdbb-restore-validate.bash

--- nfs/restore-ora-coh-nfs/restore-example/cohcdbb-restore-validate.bash
+++ nfs/restore-ora-coh-nfs/restore-example/cohcdbb-restore-validate.bash
@@ -6,4 +6,4 @@ oracle_mount_prefix=/coh/oraoim
 number_of_mount=4
 
 
-/home/oracle1/scripts/oim/rman/restore-ora-coh-oim.bash -h $backup_host -i $oracle_instance -m $oracle_mount_prefix -n $number_of_mount
+/home/oracle1/scripts/oim/rman/restore-ora-coh-nfs.bash -h $backup_host -i $oracle_instance -m $oracle_mount_prefix -n $number_of_mount
