fa0277f210d20bd2a6725ee346a3b832adb46878
Diana Yang <dianafogler@gmail.com>
2022-01-10 10:34:03 -0800

    Update aix-download-oim.bash

--- oim/aix-download-oim.bash
+++ oim/aix-download-oim.bash
@@ -8,9 +8,9 @@ fi
 /opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/aix/aix-backup-ora-coh-oim.bash
 /opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/duplicate-ora-coh-nfs/aix/aix-duplicate-ora-coh-nfs.bash
 /opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/restore-ora-coh-nfs/aix/aix-restore-ora-coh-nfs.bash
-chmod 750 backup-ora-coh-oim.bash
-chmod 750 duplicate-ora-coh-nfs.bash
-chmod 750 restore-ora-coh-nfs.bash
+chmod 750 aix-backup-ora-coh-oim.bash
+chmod 750 aix-duplicate-ora-coh-nfs.bash
+chmod 750 aix-restore-ora-coh-nfs.bash
 cd python
 /opt/freeware/bin/curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
 /opt/freeware/bin/curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneDirectory/cloneDirectory.py
