48a4406c4345e035edbc4f7974b75352583cd4a4
Diana Yang <dianafogler@gmail.com>
2022-01-10 10:32:15 -0800

    Update aix-download-oim.bash

--- oim/aix-download-oim.bash
+++ oim/aix-download-oim.bash
@@ -5,9 +5,9 @@ cd rman
 if [[ ! -d python ]]; then
    mkdir python
 fi
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/aix/backup-ora-coh-oim.bash
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/duplicate-ora-coh-nfs/aix/duplicate-ora-coh-nfs.bash
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/restore-ora-coh-nfs/aix/restore-ora-coh-nfs.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/aix/aix-backup-ora-coh-oim.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/duplicate-ora-coh-nfs/aix/aix-duplicate-ora-coh-nfs.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/restore-ora-coh-nfs/aix/aix-restore-ora-coh-nfs.bash
 chmod 750 backup-ora-coh-oim.bash
 chmod 750 duplicate-ora-coh-nfs.bash
 chmod 750 restore-ora-coh-nfs.bash
