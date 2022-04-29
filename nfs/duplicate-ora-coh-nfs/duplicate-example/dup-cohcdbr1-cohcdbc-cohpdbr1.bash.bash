60f9475022f96ff0abd9963bd9b9d1231206adfb
Diana Yang <dianafogler@gmail.com>
2022-04-29 14:26:39 -0700

    Update dup-cohcdbr1-cohcdbc-cohpdbr1.bash.bash

--- nfs/duplicate-ora-coh-nfs/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash.bash
+++ nfs/duplicate-ora-coh-nfs/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 targetconnect="<user>/<passwd>@oracle-01/cohcdbr1"
-prod_host=oracle-01
+source_host=oracle-01
 source_db=cohcdbr1
 source_pdb=cohpdbr1
 target_oraclesid=cohcdbc
-ora_set1=/home/oracle1/scripts/nfs/dup-set1-cohcdbr2.ora
 
 echo start Oracle duplication
-/home/oracle1/scripts/nfs/rman/duplicate-ora-coh-nfs.bash -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -c ${source_pdb} -l ${ora_set1} -m /coh/oraoim -n 4
+/home/oracle1/scripts/nfs/rman/duplicate-ora-coh-nfs.bash -i ${target_oraclesid} -h ${source_host} -d ${source_db} -u ${source_pdb} -t "2022-04-01 12:30:00"  -m /coh/oraoim -n 4
