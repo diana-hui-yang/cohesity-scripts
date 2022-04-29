239dd9d5947e07ad2944f45b69d3319c4c001967
Diana Yang <dianafogler@gmail.com>
2022-04-29 14:32:49 -0700

    Create dup-cohcdbr1-cohcdbr2.bash

---
+++ nfs/duplicate-ora-coh-nfs/duplicate-example/dup-cohcdbr1-cohcdbr2.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+targetconnect="<user>/<passwd>@oracle-01/cohcdbr1"
+source_host=oracle-01
+source_db=cohcdbr1
+target_oraclesid=cohcdbr3
+ora_set1=/home/oracle1/scripts/nfs/dup-set1-cohcdbr2.ora
+
+echo start Oracle duplication
+/home/oracle1/scripts/nfs/rman/duplicate-ora-coh-nfs.bash -i ${target_oraclesid} -r ${targetconnect} -h ${source_host} -d ${source_db} -t "2022-04-01 12:30:00" -m /coh/oraoim -n 4
