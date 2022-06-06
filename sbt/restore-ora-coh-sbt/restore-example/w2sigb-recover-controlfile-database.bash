07fe5aee2e8c34ae6129b2b3daca671aeabf75b3
Diana Yang <dianafogler@gmail.com>
2022-06-06 14:12:42 -0700

    Update w2sigb-recover-controlfile-database.bash

--- sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-controlfile-database.bash
+++ sbt/restore-ora-coh-sbt/restore-example/w2sigb-recover-controlfile-database.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#this script will not use the orignal Cohesity view of Oracle backups. It will use a cloned view.
+#this script will not use the orignal Cohesity view of Oracle backups. It will use a cloned view or remote restored view
 
 oracle_instance=w2sigb
 host=orawest
@@ -12,5 +12,5 @@ force=yes
 point_in_time="2020-08-02 12:00:00"
 
 # Oracle restore
-/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -i $oracle_instance -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
+/home/oracle/scripts/sbt/rman/restore-ora-coh-sbt.bash -h $host -i $oracle_instance -t "${point_in_time}" -l $restore_controlfile -j $vip_file -v $view/$host/$oracle_instance -s $sbt_code -f $force
 
