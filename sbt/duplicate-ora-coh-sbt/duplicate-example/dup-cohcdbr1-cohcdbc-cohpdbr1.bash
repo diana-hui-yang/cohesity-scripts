5db9dd59a112287f8f451a7f26790dc50ff5bbaf
Diana Yang <dianafogler@gmail.com>
2023-06-22 14:03:12 -0700

    Update dup-cohcdbr1-cohcdbc-cohpdbr1.bash

--- sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash
+++ sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbc-cohpdbr1.bash
@@ -8,8 +8,7 @@ source_pdb=cohpdbr2
 target_oraclesid=cohcdbc
 view=ora_sbt/orawest2/cohcdbr1
 cohesity_name="sac01-ftdcoh"
-ora_set=/home/oracle1/scripts/sbt/dup-set-cohpdbr1.ora
 
 
 echo start Oracle duplication
-/home/oracle1/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${targetconnect}" -y "${cohesity_name}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -l ${ora_set} -v $view -c $source_pdb
+/home/oracle1/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${targetconnect}" -y "${cohesity_name}" -b ${prod_host} -d ${source_db} -t ${target_oraclesid} -v $view -c $source_pdb
