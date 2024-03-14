19306ac5309e35b25470b087f56ce73f98a2a976
Diana Yang <dianafogler@gmail.com>
2024-03-14 11:09:07 -0700

    Update aix-download-oim.bash

--- oracle/oim/aix-download-oim.bash
+++ oracle/oim/aix-download-oim.bash
@@ -5,9 +5,9 @@ cd rman
 if [[ ! -d python ]]; then
    mkdir python
 fi
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oim/backup-ora-coh-oim/aix/aix-backup-ora-coh-oim.bash
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/duplicate-ora-coh-nfs/aix/aix-duplicate-ora-coh-nfs.bash
-/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/nfs/restore-ora-coh-nfs/aix/aix-restore-ora-coh-nfs.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oracle/oim/backup-ora-coh-oim/aix/aix-backup-ora-coh-oim.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oracle/nfs/duplicate-ora-coh-nfs/aix/aix-duplicate-ora-coh-nfs.bash
+/opt/freeware/bin/curl -O https://raw.githubusercontent.com/diana-hui-yang/rman-cohesity/master/oracle/nfs/restore-ora-coh-nfs/aix/aix-restore-ora-coh-nfs.bash
 chmod 750 aix-backup-ora-coh-oim.bash
 chmod 750 aix-duplicate-ora-coh-nfs.bash
 chmod 750 aix-restore-ora-coh-nfs.bash
