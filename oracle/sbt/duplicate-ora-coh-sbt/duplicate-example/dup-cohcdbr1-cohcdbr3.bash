4fe8ef537b69dc66d76cef0fc02870f53cdf9720
Diana Yang <dianafogler@gmail.com>
2026-09-23 07:51:10 -0700

    Update dup-cohcdbr1-cohcdbr3.bash

--- oracle/sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbr3.bash
+++ oracle/sbt/duplicate-ora-coh-sbt/duplicate-example/dup-cohcdbr1-cohcdbr3.bash
@@ -5,6 +5,7 @@ source_host=oracle-01
 target_oraclesid=cohcdbr3
 view=ora_sbt
 cohesity_name="cluster1"
+#catalogconnect="<user>/<passwd>@oracle-01:/catalog"
 
 echo start Oracle duplication
 /home/oracle1/scripts/sbt/rman/duplicate-ora-coh-sbt.bash -r "${targetconnect}" -y "${cohesity_name}" -h ${source_host} -i ${target_oraclesid} -v $view -t "2023-04-21 13:00:00"
